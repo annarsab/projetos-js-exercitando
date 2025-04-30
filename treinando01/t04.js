4de062a63b4badd120c0daeb2e9964392f36977d
Anna Paula <bueno.annarsa@gmail.com>
2025-04-30 12:56:12 -0300

    Treinando JS

---
+++ treinando01/t04.js
@@ -0,0 +1,5 @@
+var n1 = 50;
+var n2 = 60;
+var n3 = 160;
+
+console.log(`Os números informados são ${n1}, ${n2} e ${n3}.`);
---
+++ treinando01/t05.js
@@ -0,0 +1,5 @@
+var n1 = prompt("Informe um número: ");
+var n2 = Number(prompt("Informe outro número: "));
+var n3 = Number(prompt("Informe mais outro número: "));
+
+console.log(`Você informou os números ${n1}, ${n2} e ${n3}.`);
